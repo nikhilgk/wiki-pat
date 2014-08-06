ec9ebecbcfa7f0c5e04f87085ed0d4856a3116ac
ngk <ngk@nikhil.ws>
2014-08-06 18:34:48 -0400

    Added bigram handling

--- wexreader2/mr_base.py
+++ wexreader2/mr_base.py
@@ -3,6 +3,8 @@ from nltk.stem.lancaster import LancasterStemmer
 
 import re, string
 import types
+from nltk.collocations import *
+from nltk.metrics import BigramAssocMeasures
 
 from mrjob.job import MRJob
 
@@ -18,24 +20,52 @@ class BaseMR(MRJob):
         word = word.lower()
         if self.stemmer:
             try:
-                    word = self.stemmer.stem(word)
+                word = self.stemmer.stem(word)
             except:
                 return word
         return word
-        
+    
+    counter = 0
     def tokenize(self, data, yielder):
-        print data[1]
+        self.counter += 1
+        print str(self.counter)+' : ' + data[1]
         article = data[4].decode('ascii',errors='ignore').lower()
-        # document = self.pattern.sub('',data[4].decode('ascii',errors='ignore').lower().replace('\\n', '\n'))
-        tokens = set(re.findall(self.pattern, article))
-        # for token in [t.lower() for t in nltk.word_tokenize(document)]:
+        # tokens = nltk.word_tokenize(article)
+        tokens = re.findall(self.pattern, article)
+
+        #Handle Bigrams
+        num=20
+        window_size=2
+        finder = BigramCollocationFinder.from_words(tokens, window_size)
+        finder.apply_freq_filter(2)
+        finder.apply_word_filter(lambda w: len(w) < 3 or w.lower() in self.stopwords)
+        bigram_measures = BigramAssocMeasures()
+        _collocations = finder.nbest(bigram_measures.likelihood_ratio, num)
+        colloc_strings = [w1+' '+w2 for w1, w2 in _collocations]
+        for colloc in colloc_strings:
+            resp = yielder(colloc)
+            if isinstance(resp, list):
+                for item in resp: yield item
+            else:
+                yield resp
+
+        #Handle individual tokens
+        tokens=set(tokens)
+        uniquetokens = set()
         for token in tokens:
             if token in self.stopwords:
                 continue
             token = self.transform(token)
+            uniquetokens.add(token)
+        for token in uniquetokens:
             resp = yielder(token)
             if isinstance(resp, list):
                 for item in resp: yield item
             else:
                 yield resp
             
+
+
+    #             tokens = nltk.word_tokenize(child.text.encode('utf-8').lower())
+    # text = nltk.Text(tokens)
+    # text.collocations()
